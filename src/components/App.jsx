be4eda48b09ba020b69014f5b0da240be4c8a648
thearasengkh <seng.theara.kh@gmail.com>
2017-02-11 15:23:54 +0700

    add questions json data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,83 @@ import ReactDOM from 'react-dom'
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      questions : [
+        {
+          id:1,
+          text:'What is your name?',
+          choices:[
+            {
+              id:'a',
+              text:'Theara'
+            },
+            {
+              id:'b',
+              text:'Dara'
+            },
+            {
+              id:'c',
+              text:'Veasna'
+            },
+            {
+              id:'d',
+              text:'Srey Oun'
+            }
+          ],
+          correct:'a'
+        },
+        {
+          id:2,
+          text:'What is your university?',
+          choices:[
+            {
+              id:'a',
+              text:'IFL'
+            },
+            {
+              id:'b',
+              text:'RUPP'
+            },
+            {
+              id:'c',
+              text:'Setec'
+            },
+            {
+              id:'d',
+              text:'Norton'
+            }
+          ],
+          correct:'d'
+        },
+        {
+          id:3,
+          text:'What field do you take in colleage?',
+          choices:[
+            {
+              id:'a',
+              text:'Health Care'
+            },
+            {
+              id:'b',
+              text:'Computer Science'
+            },
+            {
+              id:'c',
+              text:'Accountant'
+            },
+            {
+              id:'d',
+              text:'Business'
+            }
+          ],
+          correct:'b'
+        },
+      ]
+    }
+  }
+
   render() {
     return(
         <div>
